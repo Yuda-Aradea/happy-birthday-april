fe9eafff1bd60fca14e5bad89352bec8ee038224
Yuda-Aradea <127091652+Yuda-Aradea@users.noreply.github.com>
2023-04-03 23:40:48 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*==================== Typed JS ================*/
+
+const typed = new Typed('.multiple-text', {
+  strings: ['Sayangku', 'Cintaku', 'Manisku', 'Kesayanganku', 'Kebangganku'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
